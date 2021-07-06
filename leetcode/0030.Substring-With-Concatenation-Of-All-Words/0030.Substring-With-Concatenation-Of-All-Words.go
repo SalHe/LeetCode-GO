679f032f8b3d3db7ec5d641669a9e44fe914798c
SalHe <SalHe@qq.com>
2021-07-06 23:21:22 +0800

    稍微调整（没什么见效）

--- leetcode/0030.Substring-With-Concatenation-Of-All-Words/0030.Substring-With-Concatenation-Of-All-Words.go
+++ leetcode/0030.Substring-With-Concatenation-Of-All-Words/0030.Substring-With-Concatenation-Of-All-Words.go
@@ -7,9 +7,13 @@ func findSubstring(s string, words []string) []int {
 
 	wordsCount := len(words)
 	wordsMap := make(map[string]int, wordsCount)
-	for i, word := range words {
+	for _, word := range words {
 		// 实际value目前没有用上，不过可能对wordsCount有帮助，暂时先不考虑了
-		wordsMap[word] = i
+		if count, ok := wordsMap[word]; ok {
+			wordsMap[word] = count + 1
+		} else {
+			wordsMap[word] = 1
+		}
 	}
 
 	if wordsCount > 0 {
@@ -17,7 +21,9 @@ func findSubstring(s string, words []string) []int {
 		i, upper := 0, len(s)-wordLen*wordsCount
 		for i <= upper {
 
+			// 分割出来的单词均可在words中找到
 			allExist := true
+			hitCount := 0
 
 			// 分割窗口内字符串的单词
 			// 因为目标单词的长度是固定的
@@ -30,7 +36,10 @@ func findSubstring(s string, words []string) []int {
 					finalStart = start
 				}
 				wordSplit := s[start : start+wordLen]
-				if _, ok := wordsMap[wordSplit]; !ok {
+				targetCount, ok := wordsMap[wordSplit]
+				// 如果这里发现单词不存在其实可以直接终止本窗口内的检查了
+				if !ok {
+					allExist = false
 					break
 
 					// 不存在此单词，那么窗口直接可以滑到该单词之后了（这样会漏解，可能需要另外处理才行）
@@ -38,26 +47,34 @@ func findSubstring(s string, words []string) []int {
 					// goto nextPos
 				}
 
+				temp := 1
+				hitCount++
 				if count, ok := hashCount[wordSplit]; ok {
-					hashCount[wordSplit] = count + 1
-				} else {
-					hashCount[wordSplit] = 1
+					temp = count + 1
 				}
+
+				// 倘若某个单词命中次数过多，那说明该仓库可以直接丢弃
+				if temp > targetCount {
+					allExist = false
+					break
+				}
+				hashCount[wordSplit] = temp
 			}
 
 			// 现在只要判断是否所有单词都存在即可
 			// 并找到起始位置
-
-			for _, word := range words {
-				count, ok := hashCount[word]
-				if !ok || count <= 0 {
-					allExist = false
-					break
+			if allExist && hitCount == wordsCount {
+				for _, word := range words {
+					count, ok := hashCount[word]
+					if !ok || count <= 0 {
+						allExist = false
+						break
+					}
+					hashCount[word] = count - 1
 				}
-				hashCount[word] = count - 1
 			}
 
-			if allExist {
+			if allExist && hitCount == wordsCount {
 				result = append(result, finalStart)
 			}
 
