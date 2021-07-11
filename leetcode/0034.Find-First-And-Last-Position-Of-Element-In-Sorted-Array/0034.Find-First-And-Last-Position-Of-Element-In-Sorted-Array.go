ded6639ca3ecbe0e81f5301462c365529b4b77bf
SalHe <SalHe@qq.com>
2021-07-11 23:24:31 +0800

    添加类似二分查找的实现

--- leetcode/0034.Find-First-And-Last-Position-Of-Element-In-Sorted-Array/0034.Find-First-And-Last-Position-Of-Element-In-Sorted-Array.go
+++ leetcode/0034.Find-First-And-Last-Position-Of-Element-In-Sorted-Array/0034.Find-First-And-Last-Position-Of-Element-In-Sorted-Array.go
@@ -1,6 +1,69 @@
 package leetcode
 
 func searchRange(nums []int, target int) []int {
+	if len(nums) == 0 {
+		return []int{-1, -1}
+	}
+	return searchRange_binary(nums, target, 0, len(nums)-1)
+}
+
+func searchRange_binary(nums []int, target int, start int, end int) []int {
+	if start > end {
+		return []int{-1, -1}
+	}
+	mid := start + (end-start)/2
+	if nums[mid] == target {
+		// 分别向左右找边界
+		s, e := mid, mid
+
+		// 寻找左边界（最靠右且小于[mid]的数）
+		left, right := start, mid
+		center := left + (right-left)/2
+		for left <= right {
+			if nums[center] == target {
+				s = center
+				if center == left || nums[center-1] < target {
+					break
+				}
+				right = center - 1
+			} else if center < right && nums[center+1] == target {
+				s = center + 1
+				break
+			} else {
+				left = center + 1
+			}
+			center = left + (right-left)/2
+		}
+		// 寻找右边界（最靠左且大于[mid]的数）
+		left, right = mid, end
+		center = left + (right-left)/2
+		for left <= right {
+			if nums[center] == target {
+				e = center
+				if center == right || nums[center+1] > target {
+					break
+				}
+				left = center + 1
+			} else if center > left && nums[center-1] == target {
+				e = center - 1
+				break
+			} else {
+				right = center - 1
+			}
+			center = left + (right-left)/2
+		}
+
+		return []int{s, e}
+	} else if nums[mid] > target {
+		// 说明target位于左边部分的数组
+		return searchRange_binary(nums, target, start, mid-1)
+	} else {
+		// 说明target位于右边部分的数组
+		return searchRange_binary(nums, target, mid+1, end)
+	}
+}
+
+func searchRange_linear(nums []int, target int) []int {
 	start, end := -1, -1
 	for i := 0; i < len(nums); i++ {
 		if nums[i] > target {
--- leetcode/0034.Find-First-And-Last-Position-Of-Element-In-Sorted-Array/0034.Find-First-And-Last-Position-Of-Element-In-Sorted-Array_test.go
+++ leetcode/0034.Find-First-And-Last-Position-Of-Element-In-Sorted-Array/0034.Find-First-And-Last-Position-Of-Element-In-Sorted-Array_test.go
@@ -18,10 +18,33 @@ func Test_searchRange(t *testing.T) {
 	}{
 		{
 			args: args{
+				nums:   []int{2, 2},
+				target: 2,
+			},
+			want: []int{0, 1},
+		},
+		{
+			args: args{
+				nums:   []int{1},
+				target: 1,
+			},
+			want: []int{0, 0},
+		},
+		{
+			name: "group1",
+			args: args{
 				nums:   []int{5, 7, 7, 8, 8, 10},
 				target: 8,
 			},
 			want: []int{3, 4},
+		},
+		{
+			name: "group2",
+			args: args{
+				nums:   []int{1, 2, 3, 3, 3, 3, 4, 5, 9},
+				target: 3,
+			},
+			want: []int{2, 5},
 		},
 		{
 			args: args{
@@ -37,10 +60,21 @@ func Test_searchRange(t *testing.T) {
 			},
 			want: []int{-1, -1},
 		},
+		{
+			name: "single",
+			args: args{
+				nums:   []int{0, 0, 1, 2, 3, 3, 4},
+				target: 2,
+			},
+			want: []int{3, 3},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := searchRange(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("searchRange() = %v, want %v", got, tt.want)
+			}
+			if got := searchRange_linear(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("searchRange() = %v, want %v", got, tt.want)
 			}
 		})
