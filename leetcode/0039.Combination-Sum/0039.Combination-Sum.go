8b1d16b799b21e4c9b65a433372ac8ecd7f2600c
SalHe <SalHe@qq.com>
2021-07-15 19:58:10 +0800

    DFS

--- leetcode/0039.Combination-Sum/0039.Combination-Sum.go
+++ leetcode/0039.Combination-Sum/0039.Combination-Sum.go
@@ -2,24 +2,24 @@ package leetcode
 
 // 所有数字（包括 target）都是正整数。
 // 解集不能包含重复的组合。
-func combinationSum(candidates []int, target int) [][]int {
-	// 先写出一个比较简单易懂的实现
-	var result [][]int
-	for _, candidate := range candidates {
-		if candidate == target {
-			result = append(result, []int{candidate})
-		} else if candidate < target {
-			mediaResults := combinationSum(candidates, target-candidate)
-			for _, mediaResult := range mediaResults {
-				if len(mediaResult) <= 0 || candidate > mediaResult[0] {
-					continue
-				}
-				r := make([]int, len(mediaResult)+1)
-				r[0] = candidate
-				copy(r[1:], mediaResult)
-				result = append(result, r)
-			}
+func combinationSum(candidates []int, target int) (ans [][]int) {
+	var comb []int
+	var dfs func(target, i int)
+	dfs = func(target, i int) {
+		if i == len(candidates) {
+			return
+		}
+		if target == 0 {
+			ans = append(ans, append([]int(nil), comb...))
+			return
+		}
+		dfs(target, i+1)
+		if target-candidates[i] >= 0 {
+			comb = append(comb, candidates[i])
+			dfs(target-candidates[i], i)
+			comb = comb[:len(comb)-1]
 		}
 	}
-	return result
+	dfs(target, 0)
+	return
 }
