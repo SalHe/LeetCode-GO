c971dce715b649328154745387a6ee676dad66ff
SalHe <SalHe@qq.com>
2021-10-31 22:09:06 +0800

    修正判别方式

--- leetcode/0048.Rotate-Image/0048.Rotate-Image_test.go
+++ leetcode/0048.Rotate-Image/0048.Rotate-Image_test.go
@@ -25,7 +25,7 @@ func Test_rotate(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			rotate(tt.args)
-			assert.ElementsMatch(t, tt.expected, tt.args)
+			assert.Equal(t, tt.expected, tt.args)
 		})
 	}
 }
